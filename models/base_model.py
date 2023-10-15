52a21e76381f356585649653a4ebe705f04937b2
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-10-15 09:53:38 -0700

    create basemodel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
-""" Base model class"""
-
 import uuid
 from datetime import datetime
-
 
 class BaseModel:
     """ Base model class with public instance attributes and methods """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Initialization of BaseModel instance """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    # Convert str representation of datetime to datetime object
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """ String representation of BaseModel instance """
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id,
-                                     self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """ Updates updated_at with current datetime """
@@ -31,5 +39,3 @@ class BaseModel:
         base_model_dict["created_at"] = self.created_at.isoformat()
         base_model_dict["updated_at"] = self.updated_at.isoformat()
         return base_model_dict
-
-
